13822e1f54804b6470bc9d49f48db01ad96d0de4
michelmzc <michelmzco@gmail.com>
2025-02-10 16:51:14 -0300

    finalizando tutorial

--- main.gd
+++ main.gd
@@ -6,11 +6,18 @@ var score
 func game_over():
 	$ScoreTimer.stop() 
 	$MobTimer.stop()
+	$HUD.show_game_over() 
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
-	score = 0 
+	score = 0 	
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$Music.play()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
@@ -18,6 +25,7 @@ func _on_start_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1 
+	$HUD.update_score(score)
 
 func _on_mob_timer_timeout():
 	# crear una nueva instancia de la escena Mob
