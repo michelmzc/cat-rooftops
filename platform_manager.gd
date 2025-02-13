556c5158b19346a93e05e0e004edcba00629c0d9
michelmzc <michelmzco@gmail.com>
2025-02-13 14:25:32 -0300

    agregando plataformas iniciales

--- platform_manager.gd
+++ platform_manager.gd
@@ -1,33 +1,34 @@
 extends Node
 
 @export var platform_scene: PackedScene # prefab de una plataforma
-@export var platform_spacing: float = 295.0 # distancia entre plataformas (puede variar)
+@export var platform_spacing: float = 390.0 # distancia entre plataformas (puede variar)
 @export var start_platforms: int = 5 # número de plataformas iniciales
 
-var last_platform_x = 100 # posición X de la última plataforma generada
+var last_platform_x = 10 # posición X de la última plataforma generada
 
 var player = null 
-
 
 func _ready():
 	player =  get_node("../Player") 
 	generate_starting_platforms()
 
 func _process(delta):
-	if last_platform_x - player.position.x < 500:
-		spawn_platform()
+	#if last_platform_x - player.position.x < 500:
+	#	spawn_platform()
 	
 	for platform in get_children():
-		if platform.position.x < player.position.x - 500:
+		if platform.position.x < player.position.x - 600:
 			platform.queue_free() #elimina la plataforma que quedó atrás
 	
 func generate_starting_platforms():
-	for i in range(start_platforms):
-		spawn_platform() 
+	for platform in range(start_platforms):
+		spawn_platform(last_platform_x, 512) 
+		print(last_platform_x)
 		
-func spawn_platform():
+		
+func spawn_platform(position_x, position_y):
 	var new_plaftorm = platform_scene.instantiate()
-	new_plaftorm.position = Vector2(last_platform_x + platform_spacing, 512) #randf_range(250, 400)
+	new_plaftorm.position = Vector2(position_x, position_y) #512 #randf_range(250, 400)
 	add_child(new_plaftorm)
-	last_platform_x = new_plaftorm.position.x #actualiza la última plataforma generada
+	last_platform_x = new_plaftorm.position.x + platform_spacing #actualiza la última plataforma generada
 	
