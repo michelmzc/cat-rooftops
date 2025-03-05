024ad94c57684869a13634fd8d953363f3f9710d
michelmzc <michelmzco@gmail.com>
2025-03-05 16:57:05 -0300

    agregando fondo

--- main_scene.gd
+++ main_scene.gd
@@ -5,7 +5,7 @@ extends Node
 
 @export var camera_offset_x: float = 300.0 # desplazamos la cámara a la izquierda
 
-
+  
 func _process(delta):
 	# hacer que la cámara siga al jugador solo en el eje X
 	camera.position.x = player.position.x + camera_offset_x
