7ab9f4e44a673b05811bdeaecc30482000207837
michelmzc <michelmzco@gmail.com>
2025-09-16 21:34:16 -0300

    tutorial de 30 seg completado

--- hud.gd
+++ hud.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 #notifica al nodo MainScene que el botón ha sido presionado
 signal start_game
+signal end_game
 
 @onready var start_button: Button = $UI/StartButton
 @onready var score_timer: Timer  = $UI/ScoreTimer
@@ -9,13 +10,14 @@ signal start_game
 @onready var message_timer: Timer = $UI/MessageTimer
 @onready var message_label: Label = $UI/MessageLabel
 
+var elapsed_time: int = 0
 
 func show_message(text):
 	message_label.text = text
 	message_label.show()
 	message_timer.start()
 	
-func show_game_over():
+func show_game_over() -> void:
 	show_message("Game Over")
 	# esperar hasta que termine el contador de MessageTimer
 	await message_timer.timeout
@@ -26,7 +28,7 @@ func show_game_over():
 	await get_tree().create_timer(1.0).timeout
 	start_button.show()
 
-func update_score(score):
+func update_score(score) -> void:
 	score_label.text = str(score)
 
 func _on_start_button_pressed() -> void:
@@ -34,7 +36,24 @@ func _on_start_button_pressed() -> void:
 	message_label.hide()
 	score_label.show()
 	score_timer.start()
+	elapsed_time = 0
 	start_game.emit()
 
 func _on_message_timer_timeout() -> void:
 	message_label.hide()
+
+	
+func _on_score_timer_timeout() -> void:
+	elapsed_time += 1
+	score_label.text = str(elapsed_time)
+	
+	if elapsed_time >= 30:
+		score_timer.stop()
+		_show_tutorial_complete()
+		
+func _show_tutorial_complete() -> void:
+	score_label.hide()
+	message_label.text = "Tutorial Completed!"
+	message_label.show()
+	# detener el juego
+	end_game.emit()
--- main_scene.gd
+++ main_scene.gd
@@ -1,7 +1,8 @@
 extends Node
 
 @onready var player = $GameWorld/Player 
-@onready var platforms = $GameWorld/Platforms
+@onready var hud = $HUD
+@onready var platform_manager = $PlatformManager
 @onready var camera = $Camera2D
 
 # en un comienzo el juego esta pausado, con esta variable comienza el juego
@@ -11,3 +12,9 @@ func _on_hud_start_game() -> void:
 	game_running = true
 	player.start_game()
 	camera.running = true
+
+func _ready() -> void:
+	hud.end_game.connect(_on_end_game)
+	
+func _on_end_game():
+	get_tree().paused = true
